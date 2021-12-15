0bb2d33c0d89b109f1373f25cfef7493111ade1e
Eyob14 <eyobzebene3580@gmail.com>
2021-12-15 16:05:03 +0300

    global variables added

---
+++ lib/utils/global_variable.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/add_post_screen.dart';
+import '../pages/feed_screen.dart';
+import '../pages/profile_screen.dart';
+import '../pages/search_screen.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];
